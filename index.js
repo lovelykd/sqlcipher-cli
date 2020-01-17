7dd3e804e492597bc8b2f75559bbf0697159d776
lovelykd <rbrkee@gmail.com>
2020-01-17 08:54:11 +0800

    init project

---
+++ index.js
@@ -0,0 +1,8 @@
+const program =  require('commander');
+const version = require('./package.json').version;
+
+program.version(version)
+  .usage('sqlcipher-cli -d yourdatabase.db -s yoursecretkey')
+  .requiredOption('-d, --database <database>', 'database file')
+  .requiredOption('-s, --secret <secret>', 'secret key of sqlcipher')
+  .parse(process.argv);
